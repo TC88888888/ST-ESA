a743a53467ac6127d4d5d159cdeae9fa1d2f7f21
tc <tc>
2024-06-13 20:45:50 +0200

    Update hello.py

--- TradeBot/hello.py
+++ TradeBot/hello.py
@@ -1,1 +1,12 @@
-print("Hello")
+import time
+import sys
+
+def typewriter_effect(text):
+    for char in text:
+        sys.stdout.write(char)
+        sys.stdout.flush()
+        time.sleep(0.1)
+    print()
+
+if __name__ == "__main__":
+    typewriter_effect("Hello")
